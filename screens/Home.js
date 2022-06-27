cfd31501ac73b1b9f63e176f7852fb92dd94399d
vicjuice28 <victorhugofp11@hotmail.com>
2022-06-27 14:54:07 -0600

    Finished course, with no working fonts and not working search bar

--- screens/Home.js
+++ screens/Home.js
@@ -5,6 +5,23 @@ import{COLORS,NFTData} from '../constants';
 import { NFTCard,HomeHeader,FocusedStatusBar } from '../components';
 
 const Home = () => {
+  const [nftData, setnftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if(!value.length) return setnftData(NFTData);
+
+    const filteredData = NFTData.filter((item) => 
+    item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if(filteredData.length){
+      setnftData(filteredData);
+    } else{
+      setnftData(NFTData);
+    }
+  }
+
+
   return (
     <SafeAreaView style ={{flex:1}}>
       <FocusedStatusBar background ={COLORS.primary}/>
@@ -12,11 +29,11 @@ const Home = () => {
       <View style={{flex:1}}>
         <View style = {{zIndex:0}}>
           <FlatList
-          data={NFTData}
+          data={nftData}
           renderItem={({item}) => <NFTCard data={item}/>}
           keyExtractor={(item) => item.id}
           showsVerticalScrollIndicator={false}
-          ListHeaderComponent={<HomeHeader/>}
+          ListHeaderComponent={<HomeHeader onSearch={handleSearch}/>}
           />
         </View>
         <View style={{
